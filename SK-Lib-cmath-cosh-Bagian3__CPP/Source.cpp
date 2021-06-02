d79623a95e59b069c69cea5136ca3645918c4ee9
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-06-02 20:32:41 +0700

    Add project files.

---
+++ SK-Lib-cmath-cosh-Bagian3__CPP/Source.cpp
@@ -0,0 +1,30 @@
+// CPP program to demonstrate the
+// cosh() function
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+/*	Source by Programiz
+	Modified For Learn by Rizky Khapidsyah
+	I.D.E : VS2019
+*/
+
+using namespace std;
+
+int main() {
+	int x = -4;
+	double result;
+
+	result = cosh(x);
+	cout << "cosh(-4) = " << result << endl;
+
+	// x in Degrees
+	double xDegrees = 90;
+	x = xDegrees * 3.14159 / 180;
+
+	result = cosh(x);
+	cout << "cosh(90 degrees) = " << result << endl;
+
+	_getch();
+	return 0;
+}
